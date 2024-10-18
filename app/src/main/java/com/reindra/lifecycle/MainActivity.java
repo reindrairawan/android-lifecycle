c0ec89c10787a2ecc3dc6d282a6b01f10fc58399
Reindra12 <reindrairawan@gmail.com>
2024-10-18 20:51:50 +0700

    onsaveinstancestate

--- app/src/main/java/com/reindra/lifecycle/MainActivity.java
+++ app/src/main/java/com/reindra/lifecycle/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText edtWidth, edtHeight, edtLength;
     private Button btnCalculate;
     private TextView tvResult;
+    private static final String STATE_RESULT = "state_result";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +32,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+
+
         edtWidth = findViewById(R.id.edt_width);
         edtHeight = findViewById(R.id.edt_height);
         edtLength = findViewById(R.id.edt_length);
         btnCalculate = findViewById(R.id.btn_calculate);
         tvResult = findViewById(R.id.tv_result);
+
+        if (savedInstanceState != null) {
+            String result = savedInstanceState.getString(STATE_RESULT);
+            tvResult.setText(result);
+        }
+
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String inputLength = edtLength.getText().toString().trim();
                 String inputWidth = edtWidth.getText().toString().trim();
                 String inputHeight = edtHeight.getText().toString().trim();
-                boolean isEmptyFields = false;
+
+
+                double volume = Double.parseDouble(inputLength) * Double.parseDouble(inputWidth) * Double.parseDouble(inputHeight);
+                tvResult.setText(String.valueOf(volume));
             }
         });
 
 
     }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_RESULT, tvResult.getText().toString());
+    }
 }
+
