3675788a909c1364e8bea724a37bcc0a19b99caa
Reindra12 <reindrairawan@gmail.com>
2024-10-18 20:32:28 +0700

    Lifecycle

---
+++ app/src/main/java/com/reindra/lifecycle/MainActivity.java
@@ -0,0 +1,50 @@
+package com.reindra.lifecycle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+    private TextInputEditText edtWidth, edtHeight, edtLength;
+    private Button btnCalculate;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_length);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputLength = edtLength.getText().toString().trim();
+                String inputWidth = edtWidth.getText().toString().trim();
+                String inputHeight = edtHeight.getText().toString().trim();
+                boolean isEmptyFields = false;
+            }
+        });
+
+
+    }
+}
